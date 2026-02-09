223a79f18a6293536441774b2f6b275119d3d212
frizaldi06 <rzx0980.id@gmail.com>
2026-02-09 21:42:02 +0800

    Fix relative paths for language button redirects
    Updated the href paths for language buttons to be relative.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const btnJp = document.getElementById("Jp");
 
 if (btnEn) {
   btnEn.addEventListener("click", () => {
-    window.location.href = "./index.html";
+    window.location.href = "index.html";
   });
 }
 
 if (btnJp) {
   btnJp.addEventListener("click", () => {
-    window.location.href = "./index-jp.html";
+    window.location.href = "index-jp.html";
   });
 }
+
 
