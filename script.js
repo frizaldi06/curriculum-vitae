a9200ea1c837d7cca917dc72b220b73cf8020718
frizaldi06 <rzx0980.id@gmail.com>
2026-02-09 20:19:34 +0800

    CV website prototype
    First version of CV website

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const btnEn = document.getElementById("En");
+const btnJp = document.getElementById("Jp");
+
+btnEn.addEventListener("click", () => {
+  window.location.href = "D:\ayah\index.html";
+});
+
+btnJp.addEventListener("click", () => {
+  window.location.href = "D:\ayah\index-jp.html";
+});
