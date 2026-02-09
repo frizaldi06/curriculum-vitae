e48ccec1378d2eea5f841815efbd4f534b52eaf5
frizaldi06 <rzx0980.id@gmail.com>
2026-02-09 21:15:27 +0800

    Add null checks for language button event listeners

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
 const btnEn = document.getElementById("En");
 const btnJp = document.getElementById("Jp");
 
-btnEn.addEventListener("click", () => {
-  window.location.href = "/curriculum-vitae/index.html";
-});
+if (btnEn) {
+  btnEn.addEventListener("click", () => {
+    window.location.href = "/curriculum-vitae/index.html";
+  });
+}
 
-btnJp.addEventListener("click", () => {
-  window.location.href = "/curriculum-vitae/index-jp.html";
-});
-
-
+if (btnJp) {
+  btnJp.addEventListener("click", () => {
+    window.location.href = "/curriculum-vitae/index-jp.html";
+  });
+}
